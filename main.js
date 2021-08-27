cca12f716e6e7cb1c2c668c23b21a99972fba433
Charlotte Nguyen <ph.anh.nguyenpham.l1221@gmail.com>
2021-08-27 22:18:48 +0700

    initial commit
    completed A1, A2

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1. Cho một mảng các chuỗi, viết chương trình trả về mảng mới chứa các chuỗi có độ dài lớn nhất.
+
+let input = prompt("enter a list of text, separated by comma")
+let inputArray = input.split(",")
+//let inputArray =["BINH", "HUNG", "PHUOC", "CAO", "KHANHH", "a", "aa", "aaaaaaaaaaaaaa"];
+let largestLengthArr = [];
+let largestLength = inputArray[0].length
+
+for (i = 0; i < inputArray.length; i++) {
+        if (largestLength < inputArray[i].length) {
+            largestLength = inputArray[i].length
+            largestLengthArr.push(inputArray[i])
+        } 
+}
+console.log(inputArray)
+console.log(largestLengthArr)
+
+
+
+
+// 2. Có một nhóm người đang đứng thành một hàng, để chia ra làm 2 đội từ hàng người thì quản trò chia như sao. Người đứng thứ nhất vào Team 1, người đúng thứ hai vào Team 2, người đứng thứ ba vào lại Team 1, cứ tiếp tục như thế cho đến người cuối cùng.
+    // Viết chương trình có đầu vào là một mảng chưa cân nặng của tất cả mọi người theo thứ tự hàng ban đầu và yêu cầu trả về mảng chưa tổng cân nặng của 2 team.
+
+// let weight = prompt("Enter each member's weight, seperated by comma:")
+// let weightList = weight.split(",")
+// console.log(weightList)
+
+// // let weightList = [43,43,65,76,54,56]  // T1: 162  T2: 175 sample array
+
+// let sumWeightTeam1 = 0;
+// let sumWeightTeam2 = 0;
+// totalWeight = []; 
+
+// for (i = 0; i < weightList.length; i++) {
+//     if (i % 2 == 0) {
+//         sumWeightTeam1 += Number(weightList[i]);
+//     } else {
+//         sumWeightTeam2 += Number(weightList[i]);
+//     }
+// }
+// totalWeight.push(sumWeightTeam1, sumWeightTeam2)
+// console.log(totalWeight)
